6c68f491f76268fa382ae9c63c2000fe894cf89e
PawelPyc154 <pawelpyc154@gmail.comnpm>
2020-04-22 14:48:36 +0200

    change in event model

--- models/Event.js
+++ models/Event.js
@@ -4,19 +4,66 @@ const slugify = require('slugify');
 const EventSchema = new mongoose.Schema({
   name: {
     type: String,
+    trim: true,
     required: [true, 'Please add a name'],
+    max: [50, 'Too long'],
+  },
+  describe: {
+    type: String,
+    trim: true,
+    max: [500, 'Too long'],
   },
   slug: String,
   coordinates: {
-    longitude: { type: Number, required: [true, 'Please add a longitude'] },
-    latitude: { type: Number, required: [true, 'Please add a longitude'] },
+    longitude: {
+      type: Number,
+      required: [true, 'Please add a longitude'],
+    },
+    latitude: {
+      type: Number,
+      required: [true, 'Please add a longitude'],
+    },
   },
-  place: { type: String, required: [true, 'Please add place'] },
-  province: { type: String, required: [true, 'Please add voivodeship'] },
-  time: { start: { type: Number }, end: { type: Number } },
-  type: { type: String, enum: ['dance', 'picnic'], default: 'dance' },
-  attractions: { type: [String], enum: ['dance', 'picnic'] },
-  fbList: { type: Boolean, default: false },
+  place: {
+    trim: true,
+    type: String,
+    required: [true, 'Please add place'],
+  },
+  province: {
+    type: String,
+    // enum: [
+    //   'dolnośląskie',
+    //   'kujawsko-pomorskie',
+    //   'lubuskie',
+    //   'łódzkie',
+    //   'małopolskie',
+    //   'mazowieckie',
+    //   'opolskie',
+    //   'podkarpackie',
+    //   'podlaskie',
+    //   'pomorskie',
+    //   'śląskie',
+    //   'świętokrzyskie',
+    //   'warmińsko-mazurskie',
+    //   'wielkopolskie',
+    //   'zachodniopomorskie',
+    //   '',
+    // ],
+  },
+  date: {
+    type: Number,
+    required: [true, 'Please add event time start'],
+  },
+
+  type: {
+    type: String,
+    enum: ['dance', 'picnic'],
+    default: 'dance',
+  },
+  fbList: {
+    type: Boolean,
+    default: false,
+  },
   createdAt: {
     type: Date,
     default: Date.now,

205abcaa1622c820318635f80157f1be118f8f8c
PawelPyc154 <pawelpyc154@gmail.comnpm>
2020-04-22 14:47:21 +0200

    create event add geocoder

--- controllers/events.js
+++ controllers/events.js
@@ -1,11 +1,15 @@
 const Event = require('../models/Event');
 const User = require('../models/User');
 const asyncHandler = require('../middleware/async');
+const geocoder = require('../utils/geocoder');
+const translate = require('../utils/translate');
+
 // @desc         Get all bootcamps
 // @route        GET /api/bootcamps
 // @access       Public
 exports.getEvents = asyncHandler(async (req, res, next) => {
   const events = await Event.find().populate({ path: 'user', select: 'name' });
+
   res.status(200).json({ success: true, count: events.length, data: events });
 });
 // @desc         Create bootcamp
@@ -13,6 +17,14 @@ exports.getEvents = asyncHandler(async (req, res, next) => {
 // @access       Private
 exports.createEvent = asyncHandler(async (req, res, next) => {
   req.body.user = req.user.id;
+
+  const adress = await geocoder.reverse({
+    lat: req.body.coordinates.latitude,
+    lon: req.body.coordinates.longitude,
+  });
+  req.body.place = adress[0].city;
+  req.body.province = translate(adress[0].stateCode);
+
   const event = await Event.create(req.body);
   res.status(200).json({ success: true, data: event });
 });

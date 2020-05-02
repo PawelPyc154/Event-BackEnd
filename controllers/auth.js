0fe4ebfebb7e7917e8cadfa611cd0419ef26a74f
PawelPyc154 <pawelpyc154@gmail.comnpm>
2020-05-02 20:03:25 +0200

    getMe populate user

--- controllers/auth.js
+++ controllers/auth.js
@@ -114,7 +114,10 @@ exports.login = asyncHandler(async (req, res, next) => {
 // @route        POST /api/auth/me
 // @access       Private
 exports.getMe = asyncHandler(async (req, res, next) => {
-  const user = await User.findById(req.user.id);
+  const user = await User.findById(req.user.id).populate({
+    path: 'chat.user',
+    select: 'name',
+  });
   res.status(200).json({ success: true, data: user });
 });
 
